77fc8a4a132f442a46b451ec7c8209c17c2a36f6
Gaurav Pathak <gauravpathak@192-168-1-8.tpgi.com.au>
2020-04-10 23:27:19 +1000

    pagination pokemon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import PokemonList from './PokemonList'
+import Pagination from './Pagination'
+
 import './App.css';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      console.log(res.data);
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+  }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  {pokemon.map((data, index) => {
+      return <PokemonList key={index} pokemonData={data}/>
+  })}  
+  <Pagination
+  gotoNextPage={nextPageUrl ? gotoNextPage : null}
+  gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+  />
+  </>
+  )
 }
-
 export default App;
---
+++ src/PokemonList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonList = ({pokemonData}) => {
+    return (
+        <ul>
+            <li>{pokemonData}</li>
+        </ul>
+    )
+}
+
+export default PokemonList
